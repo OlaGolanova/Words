32b3d32d30fe9c73de47c1b2c413bdcefffb1eab
Ola <musicola@mail.ru>
2023-06-16 13:30:25 +0300

    add TableRow

--- words/src/components/Table/Table.jsx
+++ words/src/components/Table/Table.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faClose, faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
-
-
 import data from '../data.json'; 
-import InputChoice from '../InputChoice/InputChoice';
+
+import TableRow from '../TableRow/TableRow';
 
 import styles from './Table.module.scss';
 
@@ -13,22 +10,6 @@ import styles from './Table.module.scss';
 
 function Table() {
 
-    const result = data.map(function(item, index) {
-        return <tr key = { item.id }>
-            <td> { index+1 } </td>
-            <td> { item.english } </td>
-            <td> { item.transcription } </td>
-            <td> { item.russian } </td>
-            <td>
-                <button className = { styles.pen }>
-                    <FontAwesomeIcon icon={faPen} />
-                </button>
-                <button className = { styles.trash }>
-                    <FontAwesomeIcon icon={faTrash} />
-                </button>
-            </td>
-        </tr>;
-    });
     return (
         <table>
             <tr>
@@ -38,23 +19,21 @@ function Table() {
                 <th>Перевод</th>
                 <th></th>
             </tr>
-            <tr className={styles.choice}>
-                <td></td>
-                <td><InputChoice/></td>
-                <td><InputChoice/></td>
-                <td><InputChoice/></td>
-                <td>
-                    <button className={styles.save}>
-                        <FontAwesomeIcon icon={faCheck} />
-                         Сохранить
-                    </button>
-                    <button className={styles.cancel}>
-                        <FontAwesomeIcon icon={faClose} />
-                    </button>
-                </td>
-            </tr>
+          
             <tbody>
-                {result}
+                {
+                    data.map(function (item,index) {
+                        return  <TableRow
+                            choice = {item.choice}
+                            key={ item.id }
+                            index = {index}
+                            english = { item.english }
+                            transcription = { item.transcription }
+                            russian = { item.russian }
+                        />;
+                    })
+                }
+
             </tbody>
         </table>
     ); 
--- words/src/components/TableRow/TableRow.jsx
+++ words/src/components/TableRow/TableRow.jsx
@@ -1,22 +1,82 @@
-// import React from 'react';
+import React from 'react';
 
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
-
-
-// import styles from './TableRow.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faTrash, faCheck, faClose } from '@fortawesome/free-solid-svg-icons';
 
 
-
-// const TableRow = () => {
-//     return (
+import styles from './TableRow.module.scss';
 
 
-//     )
-   
-    
-// };
-  
-// export default TableRow;
+
+function TableRow(props){
+
+    let result = (
+        <tr key = { props.id }>
+            <td> { props.index+1 } </td>
+            <td> { props.english } </td>
+            <td> { props.transcription } </td>
+            <td> { props.russian } </td>
+            <td>
+                <button className = { styles.pen }>
+                    <FontAwesomeIcon icon={faPen} />
+                </button>
+                <button className = { styles.trash }>
+                    <FontAwesomeIcon icon={faTrash} />
+                </button>
+            </td>
+        </tr>
+    );
+      
+    if(props.choice){
+        result = (
+            <tr key = { props.id }>
+                <td> { props.index+1 } </td>
+                <td> 
+                    <form action="" className={ styles.form }>
+                        <input 
+                            className = { styles.input }
+                            type = "text"
+                            value = { props.english }
+                        />
+                    </form>
+                </td>
+                <td> 
+                    <form action="" className={ styles.form }>
+                        <input 
+                            className = { styles.input }
+                            type = "text"
+                            value = { props.transcription }
+                        />
+                    </form>
+                </td>
+                <td> 
+                    <form action="" className={ styles.form }>
+                        <input 
+                            className = { styles.input }
+                            type = "text"
+                            value = { props.russian }
+                        />
+                    </form>
+                </td>
+                <td>
+                    <button className={styles.save}>
+                        <FontAwesomeIcon icon={faCheck} />
+                        Сохранить
+                    </button>
+                    <button className={styles.cancel}>
+                        <FontAwesomeIcon icon={faClose} />
+                    </button>
+                </td>
+            </tr> 
+        );     
+    }
+
+    return (
+        {result}
+    );
+            
+}
+
+export default TableRow;
 
 
