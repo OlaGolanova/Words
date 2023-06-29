9c5e2ecaeb93dc081e1a43a35b2e2803df9706e8
Ola <musicola@mail.ru>
2023-06-29 12:39:27 +0300

    final

--- words/src/App.js
+++ words/src/App.js
@@ -16,7 +16,7 @@ export default function App() {
             <Header />
             <div className = "app">
                 <Table />
-                {/* <Card/> */}
+                <Card/>
             </div>
             <Footer />
         </>
